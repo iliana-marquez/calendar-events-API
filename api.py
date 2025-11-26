ab1e354026156f052878eaef7cf86238d4e9d594
iliana-marquez <y5d7l1us@students.codeinstitute.net>
2025-11-26 02:59:09 +0100

    fix: add import of env for proper api_key authentication

--- api.py
+++ api.py
@@ -1,9 +1,14 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Header
 from fetch_events import fetch_events_by_period
 from datetime import date
+import os
+if os.path.isfile('env.py'):
+    import env
 
 
 app = FastAPI()
+
+API_KEY = os.environ.get("API_KEY")
 
 
 @app.get("/")
@@ -12,7 +17,12 @@ def root():
 
 
 @app.get("/api/events")
-def get_events(calendar_id: str, start_date: date, end_date: date):
+def get_events(
+    calendar_id: str, 
+    start_date: date, 
+    end_date: date,
+    api_key: str = Header(None)
+):
     """
     Fetches events from a Google Calendar.
 
@@ -24,11 +34,14 @@ def get_events(calendar_id: str, start_date: date, end_date: date):
     Returns:
     - events: List of {id, title, start, end}
     """
+    if api_key != API_KEY:
+        raise HTTPException(status_code=401, detail="Invalid API key")
+
     try:
         result = fetch_events_by_period(
             calendar_id=calendar_id,
             start_date=start_date.isoformat(),
-            end_date=end_date.isoformat()
+            end_date=end_date.isoformat(),
         )
         return result
     except Exception as e:
